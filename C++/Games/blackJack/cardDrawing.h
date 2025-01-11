bf1f430cf6e9fb131cd9bd5b14e07bf0bcba98ae
Michael Fournier <michaelfournier064@outlook.com>
2025-01-11 12:47:02 -0500

    fix(cardDrawing.h): Changed cardType values, moved random seed generated to main.cpp, and added an output statement for the value of the card drawn.

--- C++/Games/blackJack/cardDrawing.h
+++ C++/Games/blackJack/cardDrawing.h
@@ -1,31 +1,32 @@
 #ifndef cardDrawing_h
 #define cardDrawing_h
+
 #include <iostream>
 #include <array>
 #include <string>
 #include <random>
 #include <ctime>
 
-class cardDrawing {
-    private:
-const std::array<std::string, 13> cardValues
-{ "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
-    std::string cardValue {};
-    const std::array
-<std::string, 4>
-    cardTypes {"Hearts", "Diamonds", "Spades", "Clubs"};
-    std::string cardType {};
-    
-    public:
+class cards {
+private:
+    const std::array<std::string, 13> cardValues{
+        "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
+    const std::array<std::string, 4> cardTypes{
+        "Hearts", "Diamonds", "Spades", "Clubs"};
+    std::string cardValue{};
+    std::string cardType{};
 
-    cardDrawing() {
-        std::srand(std::time(0)); // Seed for randomness
-    };
-    
-    std::string getCardValue() {
+public:
+    void generateCard() {
+        // Generate random card type and value
         cardType = cardTypes[std::rand() % cardTypes.size()];
         cardValue = cardValues[std::rand() % cardValues.size()];
-    };
+    }
+
+    void cardDrawing() {
+        generateCard();
+        std::cout << "Your card is a " << cardValue << " of " << cardType << "." << std::endl;
+    }
 };
 
 #endif
